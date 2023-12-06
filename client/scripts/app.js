3ce6572dc03bfb81f68c87147d1eb8f7f3b553e4
TheSowut <sowutmail@gmail.com>
2023-12-06 17:41:13 +0200

    feat: split into FE & BE and dockerize

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,14 +1,16 @@
 window.addEventListener('load', async () => {
   document.querySelector('#test').addEventListener('click', async () => {
-    const localImg = 'http://localhost:3000/sample';
-    const doesNotExistUrl = 'http://localhost:3000/image';
+    console.log('Action initiated');
+    const localImg = 'http://localhost:8080/sample';
+    const doesNotExistUrl = 'http://localhost:8080/image';
     await setImage(doesNotExistUrl);
   });
 });
 
 const setImage = async (url) => {
   const res = await (await fetch(url)).blob();
-
+  console.log('Image has been loaded.\n Setting image.');
   const imgURL = URL.createObjectURL(res);
   document.querySelector('#image').src = imgURL;
+  console.log('Image set.');
 };
---
+++ server/scripts/README.md
@@ -0,0 +1,2 @@
+1) Launch live server for the FE
+2) Run `docker run -d -p 8080:8080 does-not-exist` for the BE

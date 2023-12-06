44f5adb07824ad7645bfadae6e28840883aa9b0b
TheSowut <sowutmail@gmail.com>
2023-12-06 18:00:59 +0200

    style: center img and style btn

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,5 +1,5 @@
 window.addEventListener('load', async () => {
-  document.querySelector('#test').addEventListener('click', async () => {
+  document.querySelector('#loadImageAction').addEventListener('click', async () => {
     console.log('Action initiated');
     const localImg = 'http://localhost:8080/sample';
     const doesNotExistUrl = 'http://localhost:8080/image';
@@ -9,8 +9,8 @@ window.addEventListener('load', async () => {
 
 const setImage = async (url) => {
   const res = await (await fetch(url)).blob();
-  console.log('Image has been loaded.\n Setting image.');
+  console.log('Image has been loaded.\nSetting image...');
   const imgURL = URL.createObjectURL(res);
-  document.querySelector('#image').src = imgURL;
+  document.querySelector('#image-person').src = imgURL;
   console.log('Image set.');
 };
